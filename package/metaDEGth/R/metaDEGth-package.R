aae8f59e0f05758044020cccf7a86546dd20f848
Adam Sardar <adam@sardar.me.uk>
2018-11-06 12:18:40 +0000

    Package description

---
+++ package/metaDEGth/R/metaDEGth-package.R
@@ -0,0 +1,10 @@
+#' metaDEGth: Combining differential expression P-values in a statistically consistent fashion whilst respecting background signal/noise trends
+#'
+#' @docType package
+#' @name metaDEGth
+#' @author Adam Sardar
+#' @import data.table
+#' @import assertthat
+#' @importFrom magrittr "%<>%"
+#' @importFrom magrittr add
+NULL
