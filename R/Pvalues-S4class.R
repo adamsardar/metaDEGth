5b06f08a8a3866c165989cf5245d00a08bf43ce9
adam sardar <adam.sardar@evotec.com>
2019-12-05 10:23:57 +0000

    Comment, but don't export

--- R/Pvalues-S4class.R
+++ R/Pvalues-S4class.R
@@ -1,10 +1,9 @@
-
+# Simple classes that enforce 
 
 setClass("Pvalues",
          representation(.Data = "numeric"),
-         validity = function(object){ return( all(is.pval(object@.Data)) ) })
-
+         validity = function(object){ return( all(is.pval( na.omit(object@.Data)) ) ) })
 
 setClass("Pvalue",
          contains = "ScalarNumeric",
-         validity = function(object){ return( is.pval(object@.Data) ) })
+         validity = function(object){ return( is.pval( na.omit(object@.Data)) ) })
