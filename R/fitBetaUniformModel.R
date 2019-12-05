eb4d544e9f0cb12ca0909260678c4cbcaa3575b4
adam sardar <adam.sardar@evotec.com>
2019-12-05 10:59:54 +0000

    Tighten up method. Covert to S4 to unlock typing properties. Work on #13

--- R/fitBetaUniformModel.R
+++ R/fitBetaUniformModel.R
@@ -6,9 +6,9 @@
 #' 
 #' Analagous to BioNet::fitBumModel.
 #' 
-#' @param pVals A numeric vector of P-values for which to perform Beta-Uniform decomposition
+#' @param pValues A numeric vector of P-values for which to perform Beta-Uniform decomposition
 #' @param nStarts How many repeats of the fitting routine should the routine run before the best LLH is picked? The default is 10, but in some cases (when there is no 'clean' P-value distribution), it might be worth running for more iterations.
-#' @return fb A list of three components: a - the shape parameter, lambda - the mixture parameter and negLL - the negative log-likelihood of the data under the model
+#' @return A BetaUniformModel object, detailign the fit
 #' @importFrom MASS fitdistr
 #' @importFrom stats na.omit coef logLik
 #' @importFrom purrr map possibly
@@ -21,58 +21,68 @@
 #' fbMod <- fitBetaUniformMixtureDistribution(pvalues$pValue_diffex)
 #' 
 #' #Note that you must subtract one from the bayes-factor in order to achieve equivilence with BioNet::scoreNodes
-#' betaUnifScored <- betaUniformScore(pvalues$pValue_diffex, fbMod, FDR = 0.01) - 1
+#' betaUnifScored <- betaUniformScore(pvalues$pValue_diffex, fbMod, FDR = 0.01)
 #' 
 #' @references Pounds, S., & Morris, S. W. (2003). Estimating the occurrence of false positives and false negatives in microarray studies by approximating and partitioning the empirical distribution of p-values. Bioinformatics
-fitBetaUniformMixtureDistribution <- function(pVals, nStarts = 10L){
-  
-  validateSingleInteger(nStarts)
-  validatePvalues(pVals)
+setGeneric("fitBetaUniformMixtureDistribution", 
+           signature = signature("pValues","nStarts"),
+           valueClass = "BetaUniformModel",
+           function(pValues, nStarts = 10L){ standardGeneric("fitBetaUniformMixtureDistribution") })
 
-  epsilon <- 1E-5
+#' @describeIn fitBetaUniformMixtureDistribution Typed method for dispatch
+setMethod("fitBetaUniformMixtureDistribution",
+          signature(pValues = "Pvalues", nStarts="ScalarInteger"),
+          function(pValues, nStarts = 10L){
+            
+            epsilon <- 1E-5
+            
+            #Fit beta-uniform distribution from a number of uniform random starting points. We need to wrap the method safely  
+            optimiseBetaUniformParams <- possibly(
+              ~ fitdistr(x = na.omit(pValues), densfun = betaUniformDensity,
+                         start = list(a = runif(1, min = epsilon, max = 3), 
+                                      lambda = runif(1, min = epsilon, max = 1-epsilon) ),
+                         lower = c(epsilon, epsilon),
+                         upper = c(3, 1-epsilon),
+                         method = "L-BFGS-B") ,
+              otherwise = NA_real_ )
+            
+            fittedBetaUniformModelParams <- map(1:nStarts, optimiseBetaUniformParams)
+            
+            failedOptimisationRuns <- ( sapply(fittedBetaUniformModelParams, class) != "fitdistr" )
+            
+            modelLLHs <- sapply(fittedBetaUniformModelParams[!failedOptimisationRuns], logLik)
+            
+            bestFit <- which.max(modelLLHs)
+            if (length(bestFit) == 0) { stop("Beta-Uniform model could not be fitted to data") }
+            if(sd(modelLLHs) > 5) warning("log-likelihoods are very diverse - it might be worth increasing the 'nStarts' parameter.")
+            
+            bestFittedBetaUniformModelParam <- fittedBetaUniformModelParams[!failedOptimisationRuns][[bestFit]]
+            
+            betaUnifMod <- new("BetaUniformModel", 
+                               a = mkScalar(coef(bestFittedBetaUniformModelParam)["a"]),
+                               lambda = mkScalar(coef(bestFittedBetaUniformModelParam)["lambda"]),
+                               pvalues = pValues,
+                               negLL = mkScalar(logLik(bestFittedBetaUniformModelParam)[1]))
+            
+            #Inspect fb for whether any parameters lie on extremas
+            if( isTRUE(all.equal(betaUnifMod@a, epsilon, tolerance = epsilon)) |
+                isTRUE(all.equal(betaUnifMod@a, 3, tolerance = epsilon)) |
+                isTRUE(all.equal(betaUnifMod@lambda, epsilon, tolerance = epsilon)) |
+                isTRUE(all.equal(betaUnifMod@lambda, 1-epsilon, tolerance = epsilon)) ){
+              
+              warning("One or both parameters are on the limit of the defined parameter space")
+            }
+            
+            return(betaUnifMod)
+          })
 
-  #Fit beta-uniform distribution from a number of uniform random starting points. We need to wrap the method safely  
-  optimiseBetaUniformParams <- possibly(
-    ~ fitdistr(x = na.omit(pVals), densfun = betaUniformDensity,
-               start = list(a = runif(1, min = epsilon, max = 3), 
-                            lambda = runif(1, min = epsilon, max = 1-epsilon) ),
-               lower = c(epsilon, epsilon),
-               upper = c(3, 1-epsilon),
-               method = "L-BFGS-B") ,
-    otherwise = NA_real_ )
-  
-  fittedBetaUniformModelParams <- map(1:nStarts, optimiseBetaUniformParams)
-
-  failedOptimisationRuns <- ( sapply(fittedBetaUniformModelParams, class) != "fitdistr" )
-  
-  modelLLHs <- sapply(fittedBetaUniformModelParams[!failedOptimisationRuns], logLik)
-
-  bestFit <- which.max(modelLLHs)
-  if (length(bestFit) == 0) { stop("Beta-Uniform model could not be fitted to data") }
-  if(sd(modelLLHs) > 5) warning("log-likelihoods are very diverse - it might be worth increasing the 'nStarts' parameter.")
-  
-  bestFittedBetaUniformModelParam <- fittedBetaUniformModelParams[!failedOptimisationRuns][[bestFit]]
-  
-  #Build an object compatible with BioNet's beta-uniform model class
-  fb <- list(a = coef(bestFittedBetaUniformModelParam)["a"],
-             lambda = coef(bestFittedBetaUniformModelParam)["lambda"],
-             pvalues = na.omit(pVals),
-             negLL = logLik(bestFittedBetaUniformModelParam)[1]) 
-  
-  fb %<>% lapply(as.numeric)
-  class(fb) <- "bum"
-  
-  #Inspect fb for whether any parameters lie on extremas
-  if( isTRUE(all.equal(fb$a, epsilon, tolerance = epsilon)) |
-      isTRUE(all.equal(fb$a, 3, tolerance = epsilon)) |
-      isTRUE(all.equal(fb$lambda, epsilon, tolerance = epsilon)) |
-      isTRUE(all.equal(fb$lambda, 1-epsilon, tolerance = epsilon)) ){
-    
-    warning("One or both parameters are on the limit of the defined parameter space")
-  }
-  
-  return(fb)
-}
+#' @describeIn fitBetaUniformMixtureDistribution Adapter method, attempts to convert numeric values to Pvalues
+setMethod("fitBetaUniformMixtureDistribution",
+          signature(pValues = "numeric", nStarts="ANY"),
+          function(pValues, nStarts=10L){callGeneric( new("Pvalues", .Data = pValues), nStarts)})
 
 
+setMethod("fitBetaUniformMixtureDistribution",
+          signature(pValues = "Pvalues", nStarts="numeric"),
+          function(pValues, nStarts){callGeneric(pValues, new("ScalarInteger",nStarts))})
 
