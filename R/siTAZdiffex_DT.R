a7274a9e60f51a8552334e86d43b83a5a4f26698
adam sardar <adam.sardar@evotec.com>
2019-12-06 16:20:35 +0000

    Including a spike-in validation study as part of the package data

---
+++ R/siTAZdiffex_DT.R
@@ -0,0 +1,19 @@
+#' Differential expression from RNAseq assay of TAZ siRNA knockdowns
+#' 
+#' GSE73555 studied genes regulated by the transcription co-factor TAZ in a lung fibroblast cell line. The authors used siRNA
+#' to knock down the TAZ gene (aka WWTR1); two different siRNAs for TAZ and a negative control (NTC) siRNA. It serves 
+#' as a perfect 'spike-in' study for us to test gene set enrichment techniques against.
+#' 
+#' Generated 6 December 2019 from the trimmed reads associated with project. These were downloaded and aligned against the human genome using
+#' Kallisto before transcript abundances were estimated using limma-voom. Finally, differential gene expression values were computed.
+#' 
+#' @docType data
+#' @usage data(siTAZdiffex_DT)
+#' 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE73555}
+#' @references \url{https://www.genecards.org/cgi-bin/carddisp.pl?gene=WWTR1}
+#' @references Noguchi S, Saito A, Mikami Y, Urushiyama H, Horie M, Matsuzaki H, et al. TAZ contributes to pulmonary fibrosis by activating profibrotic functions of lung fibroblasts. Sci Rep. 2017
+#' @references Pimentel H, Bray NL, Puente S, Melsted P, Pachter L. Differential analysis of RNA-seq incorporating quantification uncertainty. Nat Methods. 2017
+#' @references Law CW, Chen Y, Shi W, Smyth GK. voom: Precision weights unlock linear model analysis tools for RNA-seq read counts. Genome Biol. 2014
+#' @format A data.table with 14,501 rows, documenting differential gene expression of 14,475 ENSEMBL gene entities. Addition gene symbols, entrez gene IDs and uniprot annotations also included (duplicated ENSG entries result from many-to-one mappings)
+"siTAZdiffex_DT"
