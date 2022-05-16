0c0a1cdc352522901daf744ddb6751eacc6f7e7c
Adam Sardar <adam.sardar@etherapeutics.co.uk>
2022-05-16 12:33:15 +0100

    Rip out group generics - they massively obfuscate the code and for no gain

--- R/BetaUniformModel_S4Class.R
+++ R/BetaUniformModel_S4Class.R
@@ -6,8 +6,6 @@
 #' @slot lambda Mixture parameter
 #' @slot pvalues A numeric vector of the values fit by the model
 #' @slot negLLH The negative log-likelihood of the model
-#' 
-#' @importClassesFrom  Biobase ScalarNumeric
 #' @export
 setClass("BetaUniformModel",
          representation(a = "ScalarNumeric",
@@ -46,23 +44,10 @@ setMethod("convertFromBUM", signature = c(obj = "bum"),
 
 ## Distribution methods ##
 
-## Group generic for extracting protions of a distribution at particular cutoffs
-setGroupGeneric("densityPartitioning",
-                knownMembers  = c("truePositiveFraction", "falsePositiveFraction", "trueNegativeFraction", "falseNegativeFraction"),
-                def = function(obj, pValueThreshold){ NULL})
-
-# Upper bound on the the noise component of mixture distribution [Equation (3)]: π in the equations below
-setGeneric("noiseFractionUpperBound", valueClass = "ScalarNumeric", function(obj) standardGeneric("noiseFractionUpperBound"))
-
-setMethod("noiseFractionUpperBound",
-          signature(obj = "BetaUniformModel"),
-          function(obj){ return(obj@lambda + (1-obj@lambda)*obj@a) } )
-
 
 # True positive rate from M&P: A in table 1 and equation 5
 #' @export 
 setGeneric("truePositiveFraction",
-           group = "densityPartitioning",
            function(obj, pValueThreshold){ standardGeneric("truePositiveFraction") } )
 
 setMethod("truePositiveFraction",
@@ -71,23 +56,39 @@ setMethod("truePositiveFraction",
             
             Ftau <- obj@lambda*pValueThreshold + (1-obj@lambda)*pValueThreshold^obj@a
             return( Ftau - noiseFractionUpperBound(obj)*pValueThreshold ) }) # p_A(τ)= F(τ)−πτ
-      
+
+setMethod("truePositiveFraction",
+          signature = signature(obj = "ANY", pValueThreshold = "missing"),
+          function(obj, pValueThreshold) { message("Using a threshold of 0.05"); return(callGeneric(obj, 0.05 )) })
+
+#' @importFrom Biobase mkScalar
+setMethod("truePositiveFraction",
+          signature = signature(obj = "ANY", pValueThreshold = "numeric"),
+          function(obj, pValueThreshold) { return(callGeneric(obj, mkScalar(pValueThreshold)))  })
+
 
 # FALSE positive rate from M&P: C table 1 and equation 5
 #' @export 
 setGeneric("falsePositiveFraction",  valueClass = "ScalarNumeric",
-           group = "densityPartitioning",
            function(obj, pValueThreshold){ standardGeneric("falsePositiveFraction") } )
 
 setMethod("falsePositiveFraction",
           signature = signature(obj = "BetaUniformModel", pValueThreshold = "ScalarNumeric"),
           function(obj, pValueThreshold) { return( noiseFractionUpperBound(obj)*pValueThreshold ) }) # p_C= πτ 
 
+setMethod("falsePositiveFraction",
+          signature = signature(obj = "ANY", pValueThreshold = "missing"),
+          function(obj, pValueThreshold) { message("Using a threshold of 0.05"); return(callGeneric(obj, 0.05 )) })
+
+#' @importFrom Biobase mkScalar
+setMethod("falsePositiveFraction",
+          signature = signature(obj = "ANY", pValueThreshold = "numeric"),
+          function(obj, pValueThreshold) { return(callGeneric(obj, mkScalar(pValueThreshold)))  })
+
 
 # False negative rate from M&P: B table 1 and equation 5
 #' @export 
 setGeneric("falseNegativeFraction",  valueClass = "ScalarNumeric",
-           group = "densityPartitioning",
            function(obj, pValueThreshold){ standardGeneric("falseNegativeFraction") } )
 
 setMethod("falseNegativeFraction",
@@ -98,11 +99,20 @@ setMethod("falseNegativeFraction",
             
             return(1 - Ftau - (1-pValueThreshold)*noiseFractionUpperBound(obj)   ) }) # p_B(τ)=1−F(τ)−(1−τ)π
 
+setMethod("falseNegativeFraction",
+          signature = signature(obj = "ANY", pValueThreshold = "missing"),
+          function(obj, pValueThreshold) { message("Using a threshold of 0.05"); return(callGeneric(obj, 0.05 )) })
+
+#' @importFrom Biobase mkScalar
+setMethod("falseNegativeFraction",
+          signature = signature(obj = "ANY", pValueThreshold = "numeric"),
+          function(obj, pValueThreshold) { return(callGeneric(obj, mkScalar(pValueThreshold)))  })
+
+
 
 # True negative rate from M&P: D table 1 and equation 5
 #' @export 
 setGeneric("trueNegativeFraction",  valueClass = "ScalarNumeric",
-           group = "densityPartitioning",
            function(obj, pValueThreshold){ standardGeneric("trueNegativeFraction") } )
 
 setMethod("trueNegativeFraction",
@@ -113,16 +123,36 @@ setMethod("trueNegativeFraction",
             
             return( (1-pValueThreshold)*noiseFractionUpperBound(obj) ) }) # p_D(τ)=(1−τ)π
 
-# False discovery rate - of BH fame. Equation 6 from M&P
+setMethod("trueNegativeFraction",
+          signature = signature(obj = "ANY", pValueThreshold = "missing"),
+          function(obj, pValueThreshold) { message("Using a threshold of 0.05"); return(callGeneric(obj, 0.05 )) })
 
+#' @importFrom Biobase mkScalar
+setMethod("trueNegativeFraction",
+          signature = signature(obj = "ANY", pValueThreshold = "numeric"),
+          function(obj, pValueThreshold) { return(callGeneric(obj, mkScalar(pValueThreshold)))  })
+
+
+
+# Upper bound on the the noise component of mixture distribution [Equation (3)]: π in the equations below
+setGeneric("noiseFractionUpperBound", valueClass = "ScalarNumeric", function(obj) standardGeneric("noiseFractionUpperBound"))
+
+setMethod("noiseFractionUpperBound",
+          signature(obj = "BetaUniformModel"),
+          function(obj){ return(obj@lambda + (1-obj@lambda)*obj@a) } )
+
+
+
+# False discovery rate - of BH fame. Equation 6 from M&P
 setGeneric("FDRestimate",  valueClass = "ScalarNumeric",
-           group = "densityPartitioning",
            function(obj, pValueThreshold){ standardGeneric("FDRestimate") } )
 
 setMethod("FDRestimate",
           signature = signature(obj = "BetaUniformModel", pValueThreshold = "ScalarNumeric"),
           function(obj, pValueThreshold) {
             return( falsePositiveFraction(obj, pValueThreshold)/( truePositiveFraction(obj, pValueThreshold)  + falsePositiveFraction(obj, pValueThreshold) )  ) }) # P_c/(P_a + P_c)
+
+
 
 # pValueThreshold estimate - estimates a threshdold such that FDRestimate(obj, pValueThreshold) <= confidenceLevel. Equatuion 7 in Morris & Pounds
 setGeneric("pValueThresholdAtConfidence",  valueClass = "ScalarNumeric",
@@ -139,12 +169,4 @@ setMethod("pValueThresholdAtConfidence",
             return( (( noiseFractionUpperBound(obj) - confidenceLevel*obj@lambda )/(confidenceLevel*(1-obj@lambda)))^(1/(obj@a - 1))  ) }) 
 
 
-### Group-level general methods available across all density paritioning methods. This allows us to have missing values and for it to auto-convert to a NumericScalar
-setMethod("densityPartitioning",
-          signature = signature(obj = "ANY", pValueThreshold = "missing"),
-          function(obj, pValueThreshold) { message("Using a threshold of 0.05"); return(callGeneric(obj, 0.05 )) })
 
-#' @importFrom Biobase mkScalar
-setMethod("densityPartitioning",
-          signature = signature(obj = "ANY", pValueThreshold = "numeric"),
-          function(obj, pValueThreshold) { return(callGeneric(obj, mkScalar(pValueThreshold)))  })
